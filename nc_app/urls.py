a6103aee1575356e7e5c6bd6e1991e8f80e4387e
Areena Mahek <63315474+AreenaMahek@users.noreply.github.com>
2025-03-09 13:42:27 -0500

    Update urls.py

--- nc_app/urls.py
+++ nc_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
         path('api/nc_connect', NCConnectAPI.as_view(), name='nc_connect_api'),  # to connect NC with BB
         #path('api/get_datastore', GetDatastoreAPI.as_view(), name='get_datastore_api'),  # to get datastore from NC
         path('api/nc_ds_settings', NC_DS_Settings.as_view(), name='nc_ds_settings'),
+        path('api/nc_bucket_settings', NC_Bucket_Settings.as_view(), name='nc_bucket_settings'),
         path('api/upload_file', UploadFile.as_view(), name='upload_file'),
         re_path(r'^api/get_datastore$', GetDatastoreAPI.as_view(), name='get_datastore_api'),
 
